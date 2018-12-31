0bae24bd8ea0ae4b8ff28da66123ed200d10975c
Sthefano Fernandes <sthefano.fernandes@maxmilhas.com.br>
2018-12-31 00:36:47 -0200

    Fim da aula 05-01

--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -3,10 +3,15 @@ package br.com.alura.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,13 +20,16 @@ import br.com.alura.agenda.modelo.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+    private ListView listaAlunos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        Button novoAluno = findViewById(R.id.novo_aluno);
+        listaAlunos = (ListView) findViewById(R.id.lista_alunos);
 
+        Button novoAluno = findViewById(R.id.novo_aluno);
         novoAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,6 +37,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(intentVaiProFormulario);
             }
         });
+
+        registerForContextMenu(listaAlunos);
     }
 
     private void carregaLista() {
@@ -36,7 +46,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
         List<Aluno> alunos =  dao.buscaAlunos();
         dao.close();
 
-        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno> (this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
     }
@@ -45,5 +54,26 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         carregaLista();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add("Deletar");
+        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+                AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
+                dao.deleta(aluno);
+                dao.close();
+
+                carregaLista();
+
+                Toast.makeText(ListaAlunosActivity.this, "Deletado o(a) aluno(a) " + aluno.getNome(), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -70,4 +70,10 @@ public class AlunoDAO extends SQLiteOpenHelper {
 
         return alunos;
     }
+
+    public void deleta(Aluno aluno) {
+        SQLiteDatabase db = getWritableDatabase();
+        String[] params = {aluno.getId().toString()};
+        db.delete("Alunos", "id = ?", params);
+    }
 }
